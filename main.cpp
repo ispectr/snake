02eddd37531de17de402984dd48a3944238c3c36
biscuit <eyvaz.bayramov2018@gmail.com>
2023-01-25 03:26:26 +0400

    extend board class

--- main.cpp
+++ main.cpp
@@ -7,18 +7,12 @@ int main(int argc, char **argv) {
 	initscr();
 	refresh();
 	noecho();
-/*
-	int x_max, y_max;
-	getmaxyx(stdscr, y_max, x_max);
 
-	
-	WINDOW *board_win = newwin(ROWS, COLS,
-			y_max/2 - ROWS/2,
-			x_max/2 - COLS/2);
-	box(board_win, 0, 0);
-	wrefresh(board_win);
-*/
 	Board board(ROWS, COLS);
+	board.initialize();
+
+	board.add_at(5, 5, '%');
+	board.refresh();
 
 	getch();
 	endwin();
--- src/board.hpp
+++ src/board.hpp
@@ -6,14 +6,38 @@ public:
 		int x_max, y_max;
 		getmaxyx(stdscr, y_max, x_max);
 
-		WINDOW *board_win = 
+		board_win = 
 			newwin(height, width,
 					y_max/2 - height/2,
 					x_max/2 - width/2);
+	}
 
+	void initialize() {
+		clear();
+		refresh();
+	}
+
+	void add_at(int y, int x, chtype ch) {
+		mvwaddch(board_win, y, x, ch);
+	}
+
+	chtype getInput() {
+		return wgetch(board_win);
+	}
+
+	void add_border() {
 		box(board_win, 0, 0);
+	}
+
+	void clear() {
+		wclear(board_win);
+		add_border();
+	}
+
+	void refresh() {
 		wrefresh(board_win);
 	}
+
 
 private:
 	WINDOW *board_win;
